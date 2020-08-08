f8b12f18bcacf55188f6a0068735ffa0f2e00801
Emmanuel Perotto <emmanuelperotto@gmail.com>
2020-08-08 19:03:28 -0300

    initial commit

---
+++ functions/src/index.ts
@@ -0,0 +1,13 @@
+import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
+import Axios from 'axios';
+
+admin.initializeApp();
+
+export const fetchDollarRate = functions.pubsub.schedule('every 20 minutes').onRun(async (_context) => {
+  const db = admin.firestore();
+  const { data } = await Axios.get('https://api.exchangeratesapi.io/latest?base=USD&symbols=BRL')
+  const { BRL } = data.rates
+
+  await db.collection('exchange_rates').doc('dolar').set({BRL});
+});
