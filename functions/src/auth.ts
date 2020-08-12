f756d01522480b528fe9469b9ae8d9361ad3420b
Emmanuel Perotto <emmanuelperotto@gmail.com>
2020-08-11 22:54:41 -0300

    fix auth function json

--- functions/src/auth.ts
+++ functions/src/auth.ts
@@ -12,9 +12,9 @@ export function createProfile(req: Request, res: Response) {
       displayName, photoURL, providerId
     })
     .then((data) => {
-      res.status(200).json({data})
+      return res.status(200).json({data})
     })
     .catch((error) => {
-      res.status(422).json({ errorMessage: error.message })
+      return res.status(422).json({ errorMessage: error.message })
     });
 };
